b5fbcb47c8d8a61fe671fde2f94de7125e1d42b5
Mohammed Abbas <105282200+Mohammed-Abbas-0@users.noreply.github.com>
2022-06-05 16:16:34 +0200

    Time: 190 ms (42.10%), Space: 41.4 MB (46.86%) - LeetHub

--- 27-remove-element/27-remove-element.cs
+++ 27-remove-element/27-remove-element.cs
@@ -1,12 +1,8 @@
 public class Solution {
     public int RemoveElement(int[] nums, int val) {
             int countNumbersNotDuplicates = 0;
-        // Loop for all the elements in the array
         for (int i = 0; i < nums.Length; i++) {
-            // If the current element is equal to the next element, we skip
-            if (nums[i] == val) {
-                continue;
-            }
+            if (nums[i] == val)  continue;
             // We will update the array in place
             nums[countNumbersNotDuplicates] = nums[i];
             countNumbersNotDuplicates++;
