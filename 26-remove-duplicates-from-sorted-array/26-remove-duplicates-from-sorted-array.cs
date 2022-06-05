9fc75aacc920d7cc4f1604dad5bf2a3785784ef3
Mohammed Abbas <105282200+Mohammed-Abbas-0@users.noreply.github.com>
2022-06-05 16:07:40 +0200

    Time: 230 ms (29.03%), Space: 44.7 MB (81.79%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cs
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cs
@@ -2,9 +2,7 @@ public class Solution {
     public int RemoveDuplicates(int[] nums) {
         int countNumbersNotDuplicates = 0;
         for (int i = 0; i < nums.Length; i++) {
-            if (i < nums.Length-1 && nums[i] == nums[i + 1]) {
-                continue;
-            }
+            if (i < nums.Length-1 && nums[i] == nums[i + 1]) continue;
             // We will update the array in place
             nums[countNumbersNotDuplicates] = nums[i];
             countNumbersNotDuplicates++;
