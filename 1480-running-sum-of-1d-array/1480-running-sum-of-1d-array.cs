d0788dc7fa7bdad7aac557dce77ff9c97fc9d800
Mohammed Abbas <105282200+Mohammed-Abbas-0@users.noreply.github.com>
2022-07-07 22:52:55 +0200

    Time: 155 ms (82.36%), Space: 41.5 MB (73.59%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cs
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cs
@@ -1,6 +1,7 @@
 public class Solution {
+  
     public int[] RunningSum(int[] nums) {
-        int i = 1;
+   /*     int i = 1;
         while(i <= nums.Length-1)
         {
             nums[i] += nums[i-1];
@@ -9,4 +10,19 @@ public class Solution {
         return nums;
         
     }
+    */
+
+        int n = nums.Length; //finding out the length of the array using length ATTRIBUTE in java
+        int[] runningSum = new int[n];  // creating a new array with the same length as of the input
+        runningSum[0] = nums[0];  //running sum of the first element is always equals to the  1st element in input array
+        
+        int sum = nums[0] + nums[1]; //defining and using sum variable to find out running sum also finding running sum for the 2nd element of the array 
+        runningSum[1] = sum;  //assinging the running sum 
+        for(int i =2;i<n;i++ ){  // using for loop to calculate the sum beginning from 2 since whe have already found out the running sum for 0th and 1st element
+			sum += nums[i]; // calculating the sum
+			runningSum[i] = sum; //assinging the sum value in the array with respective position
+        }
+        return runningSum; //returning the sum array
+    }
+
 }
