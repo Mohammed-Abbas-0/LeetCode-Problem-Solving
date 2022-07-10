7f872b68e78058b06fea9e79b3c7d3b9cf16ff12
Mohammed Abbas <105282200+Mohammed-Abbas-0@users.noreply.github.com>
2022-07-10 06:17:18 +0200

    Time: 228 ms (25.71%), Space: 40.8 MB (68.28%) - LeetHub

---
+++ 66-plus-one/66-plus-one.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int[] PlusOne(int[] digits) {
+        int len=digits.Length;
+        for(int i=len-1;i >= 0; i--)
+        {
+            
+            if(digits[i]<9)
+            {digits[i]++; return digits;}
+             digits[i] = 0;
+            
+        }
+        int[] newArray = new int[len+1];
+        newArray[0] = 1;
+        return newArray;
+    }
+}
