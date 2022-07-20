b9e5e81e6d691c4986d53fd7b1b848dad6599da9
Mohammed Abbas <105282200+Mohammed-Abbas-0@users.noreply.github.com>
2022-07-20 19:25:42 +0200

    Time: 137 ms (29.76%), Space: 38.5 MB (84.14%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cs
+++ 242-valid-anagram/242-valid-anagram.cs
@@ -1,45 +1,25 @@
-// public class Solution {
-//     public bool IsAnagram(string s, string t) {
-//         if(s.Length != t.Length) return false;
-//         int Index;
-//         foreach(char ch in s)
-//         {
-            
-//             if(t.Contains(ch))
-//             {
-//                 Index = t.IndexOf(ch,0);
-//                 t   = t.Remove(Index,1);
-//             }
-//             else return false;
-//         }
-//         return true;
-        
-
-//     }
-// }
-
 public class Solution
 {
     public bool IsAnagram(string s, string t) 
     {
-        var dict = new Dictionary<char,int>(); 
-        foreach(var i in s)
+        if(s.Length != t.Length) return false;
+        Dictionary<char,int> Dict = new Dictionary<char,int>(); 
+        foreach(char i in s)
         {
-            if(!dict.ContainsKey(i)) 
-                dict.Add(i,0); 
-            dict[i]++;
+            if(!Dict.ContainsKey(i)) Dict.Add(i,0); 
+            Dict[i]++;
         }
         
-        foreach(var i in t)
+        foreach(char i in t)
         {
-            if(dict.ContainsKey(i))
+            if(Dict.ContainsKey(i))
             {
-                dict[i]--;
-                if(dict[i]==0)  dict.Remove(i); 
+                Dict[i]--;
+                if(Dict[i]==0)  Dict.Remove(i); 
                 
             }
             else  return false; 
         }
-        return dict.Count == 0;
+        return Dict.Count == 0;
     }
 }
