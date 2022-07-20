17a2d26c3aef5d6357f5d00f98209a3685c073a2
Mohammed Abbas <105282200+Mohammed-Abbas-0@users.noreply.github.com>
2022-07-20 19:00:19 +0200

    Time: 1813 ms (5.02%), Space: 42.6 MB (5.36%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cs
+++ 242-valid-anagram/242-valid-anagram.cs
@@ -5,7 +5,11 @@ public class Solution {
         foreach(char ch in s)
         {
             
-            if(t.Contains(ch)) t = t.Remove(t.IndexOf(ch,0),1);
+            if(t.Contains(ch))
+            {
+                Index = t.IndexOf(ch,0);
+                t   = t.Remove(Index,1);
+            }
             else return false;
         }
         return true;
@@ -13,3 +17,30 @@ public class Solution {
 
     }
 }
+/*
+public class Solution
+{
+    public bool IsAnagram(string s, string t) 
+    {
+        var dict = new Dictionary<char,int>(); 
+        foreach(var i in s)
+        {
+            if(!dict.ContainsKey(i)) 
+                dict.Add(i,0); 
+            dict[i]++;
+        }
+        
+        foreach(var i in t)
+        {
+            if(dict.ContainsKey(i))
+            {
+                
+                if(dict[i]==0)  dict.Remove(i); 
+                dict[i]--;
+            }
+            else  return false; 
+        }
+        return dict.Count == 0;
+    }
+}
+*/
