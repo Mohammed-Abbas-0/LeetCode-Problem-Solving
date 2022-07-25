686f624e9eb8c5b794374fd0553f705ca94b7e17
Mohammed Abbas <105282200+Mohammed-Abbas-0@users.noreply.github.com>
2022-07-25 03:50:33 +0200

    Time: 156 ms (87.51%), Space: 44.7 MB (13.31%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cs
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cs
@@ -1,23 +1,32 @@
 public class Solution {
     public int[] SearchRange(int[] nums, int target) {
-        int[] result  = new int[]{-1,-1};
-        if(nums.Length == 0) return result;
-        int left = 0, right=nums.Length-1;
-        while(left <= right)
-        {
-            int mid = (left+ right) / 2;
-            if(nums[mid] == target)
-            {
-                int start = mid,end = mid;
-                while(start >= 0 && nums[start] == target) start--;
-                while(end < nums.Length && nums[end] == target) end++;
-                return new int[]{start+1, end-1};
-            }
-            
-            else if(nums[mid] < target) left = mid+1;
-            else right = mid-1;
-        }
-        
-        return result;
+		int[] result = new int[]{-1,-1};
+		if (nums.Length == 0) return result;
+		result[0] = BinarySearch(nums,target);
+		if(nums[result[0]] != target) 
+		{
+			result[0] = -1;
+            return result;
+		}
+		
+		result[1] = BinarySearch(nums,target+1);
+		
+		if(nums[result[1]] != target) 
+		{
+			result[1] -= 1;
+		}
+		return result;
     }
+	
+	private int BinarySearch(int[] nums, int target)
+	{
+		int left = 0,right=nums.Length-1;
+		while(left < right)
+		{
+			int mid = (left+right)/2;
+			if(nums[mid] >= target) right = mid;
+			else left = mid+1;
+		}
+		return left;
+	}
 }
