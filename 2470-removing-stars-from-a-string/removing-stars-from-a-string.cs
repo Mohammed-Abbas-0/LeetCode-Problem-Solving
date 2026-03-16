3777008e954e5ffd9bbff72b3c0d3a6407e70c81
Mohammed Abbas <105282200+Mohammed-Abbas-0@users.noreply.github.com>
2026-03-17 01:40:02 +0200

    Time: 16 ms (48.56%) | Memory: 47.8 MB (68.66%) - LeetSync

---
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public string RemoveStars(string str) {
+        Stack<char> stack = new Stack<char>();
+foreach (var c in str)
+{
+    if (char.IsLetter(c))
+        stack.Push(c);
+    if(c == '*')
+        stack.Pop();
+
+}
+return new string(stack.Reverse().ToArray());
+    
+    }
+}
