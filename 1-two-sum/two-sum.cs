57c49d92d94af0bc3f21e8c2b70a78cbd060dbf1
Mohammed Abbas <105282200+Mohammed-Abbas-0@users.noreply.github.com>
2026-06-24 12:46:19 +0300

    Time: 4 ms (88.55%) | Memory: 50.1 MB (27.49%) - LeetSync

--- 1-two-sum/two-sum.cs
+++ 1-two-sum/two-sum.cs
@@ -1,12 +1,12 @@
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
-        Dictionary<int,int> map = new();
-        for(int i = 0; i < nums.Length; i++){
+        Dictionary<int,int> dic = new();
+        for(int i = 0; i < nums.Length; i++)
+        {
             int res = target - nums[i];
-            if(map.ContainsKey(res))
-                return new int[] {map[res], i}; 
-            map[nums[i]] = i;
-    
+            if(dic.ContainsKey(res))
+                return new int[]{dic[res],i};
+            dic[nums[i]] = i;
         }
         return new int[]{0,1};
     }
