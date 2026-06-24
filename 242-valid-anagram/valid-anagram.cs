749a16747a1fb14efa743186ffacc73381f06b7b
Mohammed Abbas <105282200+Mohammed-Abbas-0@users.noreply.github.com>
2026-06-24 13:27:13 +0300

    Time: 12 ms (44.11%) | Memory: 45.9 MB (31.27%) - LeetSync

--- 242-valid-anagram/valid-anagram.cs
+++ 242-valid-anagram/valid-anagram.cs
@@ -4,22 +4,25 @@ public class Solution {
         Dictionary<char,int> dic = new();
         for(int i = 0; i < s.Length; i++)
         {
-           if(dic.ContainsKey(s[i]))
-            dic[s[i]]++;
-        else
-            dic[s[i]] = 1;
-        }
-         for(int i = 0; i < t.Length; i++)
-        {
-            
-            if(dic.ContainsKey(t[i])){
-                dic[t[i]]--;
-             if(dic[t[i]] < 0) return false;
-            }
+            if(dic.ContainsKey(s[i]))
+                dic[s[i]]++;
             else
-                return false;
+                dic[s[i]]= 1;    
         }
 
+        for(int i = 0; i < t.Length; i++)
+        {
+             if(!dic.ContainsKey(t[i])){
+                return false;
+             }
+            if(dic[t[i]] == 0)
+                return false;
+            else{
+                dic[t[i]]--;
+            }
+            // return true;
+             
+        }
         return true;
     }
 }
