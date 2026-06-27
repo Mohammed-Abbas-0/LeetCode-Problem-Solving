22b0cddcd275cd59bb04061abbbf7a0b62e1c466
Mohammed Abbas <105282200+Mohammed-Abbas-0@users.noreply.github.com>
2026-06-27 05:17:13 +0300

    Time: 11 ms (57.61%) | Memory: 45.8 MB (31.49%) - LeetSync

--- 242-valid-anagram/valid-anagram.cs
+++ 242-valid-anagram/valid-anagram.cs
@@ -1,27 +1,25 @@
 public class Solution {
     public bool IsAnagram(string s, string t) {
+
         if(s.Length != t.Length) return false;
+
         Dictionary<char,int> dic = new();
-        for(int i = 0; i < s.Length; i++)
-        {
+        for(int i = 0; i < s.Length; i++){
             if(dic.ContainsKey(s[i]))
                 dic[s[i]]++;
             else
-                dic[s[i]]= 1;    
+                dic[s[i]] = 1;    
         }
 
         for(int i = 0; i < t.Length; i++)
         {
-             if(!dic.ContainsKey(t[i])){
-                return false;
-             }
-            if(dic[t[i]] == 0)
-                return false;
-            else{
+            if(!dic.ContainsKey(t[i]))
+                 return false;
+            else
+            {
+                if(dic[t[i]] == 0) return false;
                 dic[t[i]]--;
-            }
-            // return true;
-             
+            }   
         }
         return true;
     }
