37b1507e43050d98be521c512022c5afa969c9b6
Mohammed Abbas <105282200+Mohammed-Abbas-0@users.noreply.github.com>
2026-06-27 04:26:47 +0300

    Time: 37 ms (53.42%) | Memory: 65.8 MB (65.75%) - LeetSync

--- 15-3sum/3sum.cs
+++ 15-3sum/3sum.cs
@@ -7,23 +7,24 @@ public class Solution {
         List<IList<int>> lst = new();
         for(int i = 0; i < nums.Length;  i++)
         {
-            int left = i + 1,right = nums.Length-1;
-    if(i > 0 && nums[i] == nums[i-1]) continue;
+            int left = i+1,right = nums.Length-1;
+            if(i > 0 && nums[i] == nums[i-1]) continue;
             while(left < right)
-{
-    if(nums[i] + nums[left] + nums[right] == 0)
-{
-    lst.Add(new List<int>{nums[i], nums[left], nums[right]});
-    left++;
-    right--;
-    while(left < right && nums[left] == nums[left-1]) left++;
-    while(left < right && nums[right] == nums[right+1]) right--;
-}
-else if(nums[i] + nums[left] + nums[right] < 0)
-    left++;
-else
-    right--;
-}
+            {
+                if(nums[i] + nums[left] + nums[right] == 0)
+                {
+                    lst.Add(new List<int>{nums[i],nums[left] , nums[right]});
+                    right--;
+                    left++;
+                    while(left < right && nums[left] == nums[left-1]) left++;
+                    while(left < right && nums[right] == nums[right+1]) right--;
+                }
+                else if(nums[i] + nums[left] + nums[right] < 0)
+                    left++;
+                else
+                    right--;
+            } 
+
            
         }
         return lst;
