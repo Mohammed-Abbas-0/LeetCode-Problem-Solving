367ac36a2d8d9858f4f887aadac81f70b8fe5a98
Mohammed Abbas <105282200+Mohammed-Abbas-0@users.noreply.github.com>
2026-06-27 06:03:20 +0300

    Time: 16 ms (69.05%) | Memory: 45.3 MB (46.88%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cs
+++ 125-valid-palindrome/valid-palindrome.cs
@@ -1,21 +1,19 @@
 public class Solution {
     public bool IsPalindrome(string s) {
-        int left = 0;
-        int right = s.Length-1;
+        int left = 0,right = s.Length-1;
         while(left < right)
         {
-           if(!char.IsLetterOrDigit(s[left]))
-            left++;
-        else if(!char.IsLetterOrDigit(s[right]))
-            right--;
-        else if(char.ToLower(s[left]) != char.ToLower(s[right]))
-            return false;
-        else
-        {
-            left++;
-            right--;
+            if(!char.IsLetterOrDigit (s[left])) left++;
+            
+            else if(!char.IsLetterOrDigit (s[right])) right--;
+            
+            else if(char.ToLower(s[left]) != char.ToLower(s[right]))
+                return false;
+            else{
+                left++;
+                right--;
+            }
         }
-    }
-    return true;
+        return true;
     }
 }
