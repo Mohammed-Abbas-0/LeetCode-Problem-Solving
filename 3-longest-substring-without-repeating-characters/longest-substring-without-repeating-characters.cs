6b6bf70bfe3513c9394dc36d784a2f8b58e16080
Mohammed Abbas <105282200+Mohammed-Abbas-0@users.noreply.github.com>
2026-06-28 09:35:43 +0300

    Time: 7 ms (86.42%) | Memory: 44.1 MB (66.97%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cs
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cs
@@ -1,15 +1,15 @@
-    public class Solution {
-        public int LengthOfLongestSubstring(string s) {
-            int left = 0, currentSize = 0, maxSize = 0;
-            HashSet<int> hash = new();
-            for(int i = 0; i < s.Length; i++){
-                while(hash.Contains(s[i]))
-                {
-                    hash.Remove(s[left]);
-                    left++;
-                }
+public class Solution {
+    public int LengthOfLongestSubstring(string s) {
+        int left = 0,maxSubString=0,currentSize =0;
+        HashSet<char> hash = new();
+        for(int i = 0; i < s.Length; i++){
+            while(hash.Contains(s[i])){
+                hash.Remove(s[left]);
+                left++;
+            }
             hash.Add(s[i]);
-            maxSize = Math.Max(maxSize, i - left + 1);            }
-            return maxSize;
+            maxSubString = Math.Max(maxSubString, i - left + 1);
         }
+        return maxSubString;
     }
+}
